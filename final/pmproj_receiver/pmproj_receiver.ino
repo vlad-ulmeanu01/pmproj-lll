8eee7e4bbf01fa5fcf8894610dc6cec0c2bfb49e
vlad-ulmeanu01 <vlad.ulmeanu01@gmail.com>
2023-05-26 23:16:31 +0300

    Update sender receiver
    Primul video semi-constant cu antena!! Din pacate tot exista artefacte, chiar si cu crc-ul pornit (probabil prost)..

--- final/pmproj_receiver/pmproj_receiver.ino
+++ final/pmproj_receiver/pmproj_receiver.ino
@@ -7,22 +7,29 @@
 #define CONSECUTIVE_NEEDED_START_BYTES 32
 #define START_PICS(_b) for (int _ = 0; _ < CONSECUTIVE_NEEDED_START_BYTES; _++) Serial.write(_b);
 
-#define LORA_PACKET_SIZE 240
+#define LORA_PACKET_SIZE 220
 #define LORA_SS 5
 #define LORA_RESET 14
 #define LORA_DIO0 2
 
-int pack[LORA_PACKET_SIZE];
-volatile bool justReceivedPack = false;
+uint8_t pack[LORA_PACKET_SIZE];
+volatile bool justReceivedPack = false, isCrcOk = true, isTxComplete = true;
 volatile int packetSize = 0;
 
 void onReceive(int psz) {
     packetSize = psz;
-    for (int i = 0; i < psz; i++) {
-        pack[i] = LoRa.read();
-    }
-
     justReceivedPack = true;
+    isCrcOk = true;
+}
+
+void onTxDone() {
+    isTxComplete = true;
+    LoRa.receive();
+}
+
+void onCrcError() {
+    justReceivedPack = true;
+    isCrcOk = false;
 }
 
 void setup() {
@@ -35,24 +42,50 @@ void setup() {
         delay(500);
     }
 
-    Serial.printf("\n");
-
     LoRa.setSyncWord(0xE2);
-    LoRa.onReceive(onReceive); ///register the receive callback.
-    LoRa.receive(); ///put the radio into receive mode.
-    
-    Serial.printf("LoRa Initializing OK!\n");
+    LoRa.onReceive(onReceive); ///receive callback. daca primesc un pachet cu crc ok.
+    LoRa.receive(); ///trec pe modul listening. ies automat cand dau beginPacket. trebuie sa intru la loc dupa endPacket.
+    LoRa.onTxDone(onTxDone); ///apelat cand am terminat de transmis.
+    LoRa.onCrcError(onCrcError); ///daca primesc un pachet cu crc busit.
+    LoRa.enableCrc();
+
+    Serial.printf("\nLoRa Initializing OK!\n");
 
     START_PICS(START_BYTE)
 }
 
 void loop() {
-    if (justReceivedPack) {
-        //Serial.printf("packetSize = %d. (%d, %d, %d, %d)\n", packetSize, pack[0], pack[1], pack[2], pack[3]);
-        for (int i = 0; i < packetSize; i++) {
-            Serial.write(pack[i]);
-        }
+    if (!isTxComplete) {
+        return;
+    }
 
+    if (justReceivedPack) {
         justReceivedPack = false;
+
+        if (isCrcOk) {
+            for (int i = 0; i < packetSize; i++) {
+                pack[i] = LoRa.read();
+            }
+    
+            for (int i = 0; i < packetSize; i++) {
+                Serial.write(pack[i]);
+            }
+
+            ///raspund cu ack afirmativ <=> astept urmatorul pachet.
+            isTxComplete = false;
+            LoRa.beginPacket();
+            LoRa.write(0);
+            LoRa.endPacket();
+        } else {
+            while (LoRa.available()) {
+                LoRa.read();
+            }
+
+            ///raspund cu ack negativ <=> astept pachetul curent din nou.
+            isTxComplete = false;
+            LoRa.beginPacket();
+            LoRa.write(1);
+            LoRa.endPacket();
+        }
     }
 }
--- final/pmproj_sender/pmproj_sender.ino
+++ final/pmproj_sender/pmproj_sender.ino
@@ -11,88 +11,65 @@ HardwareSerial CustomPort(2); // use UART2
 
 #define STATE_IDLE 0 ///nu am comandat inca primirea unei imagini de la ESP32-CAM.
 #define STATE_GET_SIZE 1 ///am comandat primirea unei imagini si astept primii 4 bytes, care imi spun marimea imaginii.
-#define STATE_GET_BYTES 2 ///inca astept sa primesc toti bytes-ii din imagine.
+#define STATE_GET_BYTES 2 ///inca astept sa primesc toti bytes-ii din imagine. ii pun intr-un buffer. nu vreau sa folosesc si lora in acelasi timp.
+#define STATE_SEND_LORA 3 ///transmit bytes-ii din buffer.
+#define STATE_WAIT_TX 4 ///astept sa fi trimis mesajul.
+#define STATE_WAIT_ACK 5 ///am trimis niste bytes, astept ack.
 
-#define LORA_PACKET_SIZE 200 ///payload-ul are maxim 240 bytes per pachet.
+#define LORA_PACKET_SIZE 220 ///payload-ul are maxim 220 bytes per pachet.
 #define LORA_SS 5
 #define LORA_RESET 14
 #define LORA_DIO0 2
 
-uint8_t ch;
-int state = 0;
+#define LIMITED_IMBUFF_MAX_SIZE 65536
+
+uint8_t *limited_imbuff;
+int limitedImbuffSize = 0, limitedImbuffIndex = 0, limitedImbuffPrevIndex = 0;
 int remainingImageBytes = 0, receivedSizeBytes = 0;
 
-int lora_buffer_counter = 0;
+volatile int packetSize = 0, state = STATE_IDLE;
+volatile bool justReceivedAck = false, isCrcOk = true;
 
-//volatile bool lora_ack = false;
-//void onTxDone() {
-//    lora_ack = true;
-//}
-
-void lora_wait_ack() {
-//    delay(10);
-//    lora_ack = false;
-//    while (!lora_ack) {
-//        delay(5);
-//    }
-
-    int packetSize = LoRa.parsePacket();
-    unsigned long now;
-    while (packetSize == 0 && millis() - now < 1000) {
-        packetSize = LoRa.parsePacket();
-    }
-    
-    while (LoRa.available() > 0) {
-        LoRa.read();
-    }
+void onReceive(int psz) {
+    packetSize = psz;
+    justReceivedAck = true;
+    isCrcOk = true;
 }
 
-void lora_add_byte(uint8_t x) {
-    if (lora_buffer_counter == 0) {
-        LoRa.beginPacket();
-    }
-
-    LoRa.write(x);
-    lora_buffer_counter++;
-    Serial.printf("(lora_add_byte) lora_buffer_counter = %d\n", lora_buffer_counter);
-
-    if (lora_buffer_counter >= LORA_PACKET_SIZE) {
-        LoRa.endPacket();
-        Serial.printf("(lora_add_byte) Sent packet.\n");
-        
-        lora_wait_ack();
-        Serial.printf("(lora_add_byte) got ack.\n");
-        
-        lora_buffer_counter = 0;
-    }
+void onTxDone() {
+    state = STATE_WAIT_ACK;
+    LoRa.receive();
 }
 
-void lora_force_flush() {
-    if (lora_buffer_counter > 0) {
-        LoRa.endPacket();
-        Serial.printf("(lora_force_flush) Sent packet.\n");
-
-        lora_wait_ack();
-        Serial.printf("(lora_force_flush) got ack.\n");
-    }
+void onCrcError() {
+    justReceivedAck = true;
+    isCrcOk = false;
 }
 
 void setup() {
     Serial.begin(115200);
 
-    Serial.println("LoRa Sender");
+    Serial.printf("LoRa Sender\n");
 
     LoRa.setPins(LORA_SS, LORA_RESET, LORA_DIO0);
-    //LoRa.onTxDone(onTxDone); ///!!!tb onTxDone pe sender.
+
+    LoRa.setSyncWord(0xE2);
+    LoRa.onReceive(onReceive);
+    LoRa.receive();
+    LoRa.onTxDone(onTxDone);
+    LoRa.onCrcError(onCrcError);
+    LoRa.enableCrc();
 
     while (!LoRa.begin(433E6)) {
         Serial.print(".");
         delay(500);
     }
 
-    LoRa.setSyncWord(0xE2);
+    Serial.printf("\nLoRa Initializing OK!\n");
 
-    Serial.printf("LoRa Initializing OK!\n");
+    limited_imbuff = (uint8_t *)malloc(LIMITED_IMBUFF_MAX_SIZE);
+    assert(limited_imbuff);
+    Serial.printf("Alloc ok!\n");
 
     CustomPort.begin(115200, SERIAL_8N1, 16, 17); ///RX, TX.
     CustomPort.setRxBufferSize(65536);
@@ -106,6 +83,7 @@ void setup() {
     bool canStart = false;  
     int consecutiveStartBytes = 0;
 
+    uint8_t ch;
     while (!canStart) {
         while (CustomPort.available() > 0 && consecutiveStartBytes < CONSECUTIVE_NEEDED_START_BYTES) {
             ch = CustomPort.read();
@@ -126,32 +104,92 @@ void setup() {
 }
 
 void loop() {
+    uint8_t ch;
     if (state == STATE_IDLE) {
         CustomPort.write(0); ///doar scriu un caracter.
+        
+        limitedImbuffSize = 0;
+        limitedImbuffIndex = 0;
+        limitedImbuffPrevIndex = 0;
+        
         remainingImageBytes = 0;
         receivedSizeBytes = 0;
         state = STATE_GET_SIZE;
     } else if (state == STATE_GET_SIZE) {
         while (CustomPort.available() > 0 && receivedSizeBytes < 4) {
             ch = CustomPort.read();
-            lora_add_byte(ch);
+            limited_imbuff[limitedImbuffSize] = ch;
+            limitedImbuffSize++;
+
             receivedSizeBytes++;
             remainingImageBytes = (remainingImageBytes << 8) | ch;
         }
 
         if (receivedSizeBytes >= 4) {
+            Serial.printf("remainingImageBytes = %d\n", remainingImageBytes);
             state = STATE_GET_BYTES;
         }
-    } else { ///STATE_GET_BYTES.
+    } else if (state == STATE_GET_BYTES) {
         while (CustomPort.available() > 0 && remainingImageBytes > 0) {
             ch = CustomPort.read();
-            lora_add_byte(ch);
+            if (limitedImbuffSize < LIMITED_IMBUFF_MAX_SIZE) {
+                limited_imbuff[limitedImbuffSize] = ch;
+                limitedImbuffSize++;
+            }
+
             remainingImageBytes--;
         }
 
         if (remainingImageBytes <= 0) {
-            lora_force_flush();
+            ///daca am depasit LIMITED_IMBUFF_MAX_SIZE, modific si remainingImageBytes ca sa pot sa trec ulterior si la alta imagine.
+            if (limitedImbuffSize >= LIMITED_IMBUFF_MAX_SIZE) {
+                limited_imbuff[0] = 0;
+                limited_imbuff[1] = 0;
+                limited_imbuff[2] = 0xFF;
+                limited_imbuff[3] = 0xFB;
+            }
+
+            state = STATE_SEND_LORA;
+        }
+    } else if (state == STATE_SEND_LORA) {
+        if (limitedImbuffIndex < limitedImbuffSize) {
+            state = STATE_WAIT_TX;
+            justReceivedAck = false;
+            LoRa.beginPacket();
+            limitedImbuffPrevIndex = limitedImbuffIndex;
+            for (int i = 0; limitedImbuffIndex < limitedImbuffSize && i < LORA_PACKET_SIZE; limitedImbuffIndex++, i++) {
+                LoRa.write(limited_imbuff[limitedImbuffIndex]);
+            }
+            LoRa.endPacket();
+
+            Serial.printf("ind %d size %d\n", limitedImbuffIndex, limitedImbuffSize);
+        } else {
             state = STATE_IDLE;
+        }
+    } else if (state == STATE_WAIT_TX) {
+        ///astept callback onTxDone.
+    } else if (state == STATE_WAIT_ACK) {
+        if (justReceivedAck) {
+            justReceivedAck = false;
+            state = STATE_SEND_LORA;
+
+            if (isCrcOk) {
+                ch = LoRa.read();
+                while (LoRa.available()) {
+                    LoRa.read();
+                }
+
+                if (ch == 0) {
+                    Serial.printf("ack succes.\n");
+                    ///ack succes. pot sa trec la urmatoarea parte.
+                } else {
+                    Serial.printf("ack fail 1.\n");
+                    limitedImbuffIndex = limitedImbuffPrevIndex; ///ack fail. trimit din nou ultima parte.
+                }
+            } else {
+                Serial.printf("ack fail 2.\n");
+                limitedImbuffIndex = limitedImbuffPrevIndex; ///nici macar ack-ul nu este consistent. trimit din nou ultima parte.
+            }
         }
     }
 }
